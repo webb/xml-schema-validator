e82e45c79aa9f0c5306f88a50b94b1bfeee04ecd
Matt Ward <matthew.ward@gtri.gatech.edu>
2018-11-30 15:01:35 -0500

    Initial commit for SAXParserFactory features.

--- src/main/java/org/gtri/niem/xml_schema_validator/XMLCatalogParser.java
+++ src/main/java/org/gtri/niem/xml_schema_validator/XMLCatalogParser.java
@@ -15,16 +15,23 @@ import javax.xml.catalog.CatalogResolver;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParserFactory;
 import javax.xml.parsers.SAXParser;
+import javax.xml.XMLConstants;
 
 public class XMLCatalogParser
 {
   private static final String CATALOG_FILE = CatalogFeatures.Feature.FILES.getPropertyName();
+  private static final String CATALOG_PREFER = CatalogFeatures.Feature.PREFER.getPropertyName();
 
   public XMLCatalogParser(List<String> catalogURIs, String schemaLocations) {
     try {
       SAXParserFactory spf = SAXParserFactory.newInstance();
       spf.setNamespaceAware(true);
       spf.setXIncludeAware(true);
+      // TODO: set ACCESS constants to "file" string
+      spf.setFeature(XMLConstants.ACCESS_EXTERNAL_DTD, false);
+      spf.setFeature(XMLConstants.ACCESS_EXTERNAL_SCHEMA, false);
+      spf.setFeature(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, false);
+      spf.setFeature(XMLConstants.USE_CATALOG, true);
       SAXParser parser = spf.newSAXParser();
 
       if (catalogURIs.size() > 0) {
@@ -37,13 +44,14 @@ public class XMLCatalogParser
           CatalogResolver resolver = CatalogManager.catalogResolver(CatalogFeatures.defaults(),
                   catalogUriList.toArray(new URI[catalogUriList.size()]));
           parser.setProperty(CATALOG_FILE, resolver);
+          parser.setProperty(CATALOG_PREFER, "system");
       }
     }
     catch (SAXException exception) {
-      Logger.getInstance().error("SAXException: ", exception);
+      Logger.getInstance().error("SAXException", exception);
     }
     catch (ParserConfigurationException exception) {
-      Logger.getInstance().error("invalid configuration: ", exception);
+      Logger.getInstance().error("invalid configuration", exception);
     }
   }
 
